f8680f97851a792975c36a0310bf855e8942607d
Weslley Lima <weslley.lima@zup.com.br>
2021-03-09 16:38:57 -0300

    Criar pacote services e classe de serviço

---
+++ src/main/java/br/com/zup/pet/services/PetService.java
@@ -0,0 +1,14 @@
+package br.com.zup.pet.services;
+
+import br.com.zup.pet.model.Pet;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PetService {
+    private static List<Pet> animais= new ArrayList<>();
+
+
+}
