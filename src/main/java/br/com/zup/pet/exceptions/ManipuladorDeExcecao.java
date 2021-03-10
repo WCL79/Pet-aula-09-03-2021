d681236b758ff6946b72ab3acd7fa3329cd2bf49
Weslley Lima <weslley.lima@zup.com.br>
2021-03-10 08:35:12 -0300

    Criar pacote exceptions para alocar classe ManipuladorDeExececao cujo objetivo à manipulação de erros

---
+++ src/main/java/br/com/zup/pet/exceptions/ManipuladorDeExcecao.java
@@ -0,0 +1,6 @@
+package br.com.zup.pet.exceptions;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ManipuladorDeExcecao extends ResponseEntityExceptionHandler {
+}
